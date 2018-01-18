b59b46465f7d019f519f6f110f663783cfc8f5e5
Harley Inbody <hinbody@gmail.com>
2018-01-18 15:14:59 -0500

    adds some comments to the SimpleClass file for future reference

--- SimpleClass.php
+++ SimpleClass.php
@@ -1,7 +1,9 @@
 <?php
 class SimpleClass {
+  #class properties are defined using keywords: public, protected, or private
   public $var1 = "me";
   public $var2 = "also me";
+  #properites can be initialized with a heredoc or a nowdoc
   public $var3 = <<<EOT
 This variable has a lot of text
 that is
@@ -11,6 +13,7 @@ EOT;
 }
 
 $stuff = new SimpleClass();
+#accessing the class properites to print them to the screen
 echo $stuff->var1 . PHP_EOL;
 echo $stuff->var2 . PHP_EOL;
 echo $stuff->var3 . PHP_EOL;
