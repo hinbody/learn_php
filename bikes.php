2a7f3146b47e67f4a4f42157681565b8e3c378cf
Harley Inbody <hinbody@gmail.com>
2018-01-26 12:16:04 -0500

    adds bikes.php to explore creating and extending classes

---
+++ bikes.php
@@ -0,0 +1,33 @@
+<?php
+
+class Bike
+{
+  public $wheel_size;
+  public $frame_color;
+
+  public function paint_frame($new_color) {
+    $this->frame_color = $new_color;
+  }
+}
+
+class Bmx extends Bike
+{
+  public $pegs = 0;
+
+  public function add_pegs($pegs){
+    $this->pegs = $pegs;
+  }
+
+}
+
+$s125r = new Bmx();
+$s125r->frame_color = "trans red";
+echo "Original frame color of 125r is " . $s125r->frame_color . PHP_EOL;
+$s125r->paint_frame("black");
+echo "New frame color of 125r is " . $s125r->frame_color . PHP_EOL;
+
+$intrikat = new Bmx();
+echo "Originally the intrikat had " . $intrikat->pegs . " pegs" . PHP_EOL;
+$intrikat->add_pegs(4);
+echo "The intrikat now has " . $intrikat->pegs . " pegs" . PHP_EOL;
+?>
